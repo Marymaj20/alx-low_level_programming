79e8b7a61fff3edb04d716c9a8eadf8f856c0281
Marymaj20 <nmajumary20@gmail.com>
2023-06-12 19:23:29 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 			text_len++;
 	}
 
-	File_des = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	file_des = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(file_des, text_content, text_len);
 
 	if (file_des == -1 || w == -1)
